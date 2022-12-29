65b79e7a1013cc692a92c6a5b409f5bf1e7a2fb7
Pasq097 <s294872@studenti.polito.it>
2022-12-29 23:45:16 +0100

    lab 9 punti 1 e 2, optimal power 0.9 mW (maybe correct)

--- core/line.py
+++ core/line.py
@@ -15,9 +15,9 @@ class Line:
         self._n_amplifiers = None       # Calculate this number with the line length, one amplifier for 80 km
         self._gain = 16  # dB
         self._noise_figure = 5.5  # dB
-        self._alpha_dB = 0.2 / 10**3      # dB / km
-        self._beta = 2.13 * 10**-26  # ps^2/km
-        self._gamma = 1.27e-3         # Wm-1
+        self._alpha_dB = 0.2 / 10**3    # dB / m
+        self._beta = 2.13 * 10**-26  # s^2/m
+        self._gamma = 1.27 * 10**-3       # Wm-1
 
     @property  # Getter
     def label(self):
@@ -72,8 +72,8 @@ class Line:
         f = 193.414 * 10**12  # THz
         B_n = 12.5 * 10**9  # GHz
         h = 6.62607015 * 10**-34
-        noise_figure_lin = 10 ** (self._noise_figure/20)
-        gain_lin = 10 ** (self._gain/20)
+        noise_figure_lin = 10 ** (self._noise_figure/10)
+        gain_lin = 10 ** (self._gain/10)
 
         ASE = number_of_amplifiers * (h * f * B_n * noise_figure_lin * (gain_lin - 1))
 
@@ -83,24 +83,43 @@ class Line:
         # in linear units
         pi = math.pi
         N_ch = 10                           # number of channels
-        alpha_lin = self._alpha_dB/(10 * math.log10(math.e))
+        alpha_lin = self._alpha_dB/(20 * math.log10(math.e))
         L_eff = 1 / (2 * alpha_lin)
         B_n = 12.5 * 10**9
-        beta = self._beta
-        ni_nli = (16 / 27*pi) * math.log10((pi**2/2) * ((beta * R_s**2)/alpha_lin) * N_ch ** (2 * (R_s / delta_f))) * alpha_lin/beta * self._gamma ** 2 * L_eff ** 2 / R_s ** 3
+        beta = abs(self._beta)
+        a = (math.pi**2) * beta * (R_s**2) * (N_ch**((2*R_s)/delta_f))
+        b = 2 * alpha_lin
+        ni_nli = (16 / (27*pi)) * (math.log10(a/b) * (alpha_lin/beta) * (
+                (self._gamma ** 2) * (L_eff ** 2) / (R_s ** 3)))
         NLI = P_ch**3 * ni_nli * n_span * B_n
 
-        return NLI
+        return NLI, ni_nli
 
     def noise_generation(self, number_of_amplifiers,  R_s, delta_f, P_ch):
 
         # 1e-9 * signal_power * length
         # noise_power = length * (10 ** (-9)) * signal_power
         noise_contribution_1 = self.ase_generation(number_of_amplifiers)
-        noise_contribution_2 = self.nli_generation(R_s, delta_f, P_ch, number_of_amplifiers)
+        noise_contribution_2 = self.nli_generation(R_s, delta_f, P_ch, number_of_amplifiers)[0]
+        ni_nli = self.nli_generation(R_s, delta_f, P_ch, number_of_amplifiers)[1]
         noise_power = noise_contribution_1 + noise_contribution_2
         # print(noise_power)
-        return noise_power
+        return noise_power, ni_nli
+
+    def optimized_launch_power(self, ni_nli):
+        # determination of the optimal launch power
+        # we have a Loss of 16 dB (same as the Gain)
+        B_n = 12.5 * 10 ** 9
+        loss_lin = 10 ** (self._gain/10)
+        F = 10 ** (self._noise_figure/10)
+        B_n = 12.5 * 10 ** 9
+        h = 6.62607015 * 10 ** -34
+        f = 193.414 * 10 ** 12
+        P_base = h * f * B_n
+        a = (P_base * loss_lin * F)
+        b = (2 * B_n * ni_nli)
+        P_opt = (a/b)**(1/3)
+        return P_opt
 
     def propagate(self, light_path):
         # if I'm on a line e.g. AB I can only go on a successive node e.g. B it's simpler thant node propagate method
@@ -113,7 +132,11 @@ class Line:
             self._successive[node].propagate(light_path)
         # self._state = 0
         # we have to "feed" the method the length of the current line
-        noise_power = self.noise_generation(self._n_amplifiers, light_path.Rs, light_path.df,  light_path.signal_power)
+        x_1 = self.noise_generation(self._n_amplifiers, light_path.Rs, light_path.df,  light_path.signal_power)
+        noise_power = x_1[0]
+        nli = x_1[1]
+        popt = self.optimized_launch_power(nli)
+        # print(popt)
         light_path.update_noise_power(noise_power)
         latency = self.latency_generation(self._length)
         light_path.update_latency(latency)
